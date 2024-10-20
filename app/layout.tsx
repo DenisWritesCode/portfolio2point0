044a231cd8cfa0c57baf6ceb3121a7c5ef397c48
DenisWritesCode <deniswritescode@gmail.com>
2024-10-20 21:24:14 +0300

    001: Add theme toggle to Navbar and entire website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import NavBar from "@/components/ui/NavBar";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,13 +31,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <div className="min-h-screen flex flex-col items-center justify-between ">
-          <header className="w-full sticky top-0 left-0 p-8 bg-foreground">
+          <header className="w-full sticky top-0 left-0 p-4 bg-background text-foreground border-b border-foreground">
             <NavBar />
           </header>
           <div className="p-4 bg-background min-h-screen w-full">{children}</div>
           <Footer />
         </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -1,12 +1,47 @@
+"use client";
+
 import Link from "next/link";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+ 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 function NavBar() {
+
+  const { setTheme } = useTheme();
+
   return (
-    <div className="flex justify-between items-center text-white">
+    <div className="flex justify-between items-center">
       <Link href={"/"}>Home</Link>
       <Link href={"/projects"}>Projects</Link>
       <Link href={"/blog"}>Blog</Link>
       <Link href={"/contact"}>Contact</Link>
+      <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
     </div>
   );
 }
