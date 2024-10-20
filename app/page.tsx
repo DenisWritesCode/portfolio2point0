f8402a2fa4d1423e391b749aa2fd90857794ace4
DenisWritesCode <deniswritescode@gmail.com>
2024-10-20 17:53:04 +0300

    001: Add all required sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,39 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import EducationAndWork from "@/components/ui/EducationNWork";
+import Footer from "@/components/ui/Footer";
+import NavBar from "@/components/ui/NavBar";
+import Services from "@/components/ui/Services";
+import SkillSet from "@/components/ui/SkillSet";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
-    <header className="w-full sticky top-0 left-0 p-8 bg-slate-800">
-      header
-    </header>
-    <main className="bg-slate-200 min-h-screen w-full">
-      <h1 className="text-3xl font-bold underline text-blue-600">
-        Hello world!
-      </h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
+      <header className="w-full sticky top-0 left-0 p-8 bg-slate-800">
+        <NavBar />
+      </header>
+      <main className="bg-slate-200 min-h-screen w-full">
+        <h1 className="text-3xl font-bold underline text-blue-600">
+          Hello world!
+        </h1>
+        <div>
+          <Button>Click me</Button>
+        </div>
 
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <p>Hello, world from Framer Motion!</p>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <p>Hello, world from Framer Motion!</p>
+          <Services />
+          <SkillSet />
+          <EducationAndWork />
+        </motion.div>
       </main>
-      <footer>
-        Footer.
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ components/ui/EducationNWork.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EducationAndWork() {
+  return (
+    <div>EducationAndWork</div>
+  )
+}
+
+export default EducationAndWork
