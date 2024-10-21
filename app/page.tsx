3ac5c4941f354a3600e78e4d0c7a78c723f27c3b
DenisWritesCode <deniswritescode@gmail.com>
2024-10-21 13:43:11 +0300

    001: Showed Footer some love

--- app/page.tsx
+++ app/page.tsx
@@ -11,28 +11,135 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col space-y-5 text-foreground">
-        <h1 className="text-3xl font-bold underline bg-primary text-primary-foreground">
-          Hello world!
-        </h1>
-        <div>
-          <Button><Link href={"/contact"}>Contact</Link></Button>
-        </div>
+      <h1 className="text-3xl font-bold underline bg-primary text-primary-foreground">
+        Hello world!
+      </h1>
+      <div>
+        <Button>
+          <Link href={"/contact"}>Contact</Link>
+        </Button>
+      </div>
 
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <p>Hello, world from Framer Motion!</p>
+        <Services />
+        <SkillSet />
+        <EducationAndWork />
+        <section
+          id="projects"
+          className="resume bg-gray-800 text-gray-100 pb-2"
         >
-          <p>Hello, world from Framer Motion!</p>
-          <Services />
-          <SkillSet />
-          <EducationAndWork />
-        </motion.div>
-
-        <Image src={"/logos/BlackCircle.png"} alt="Black Circle" width={250} height={250} />
-        <Image src={"/logos/BlackOnGray.png"} alt="Black on Gray" width={250} height={250} />
-        <Image src={"/logos/GrayCircle.png"} alt="Gray Circle" width={250} height={250} />
-        <Image src={"/logos/WhiteOnBlack.png"} alt="White on Black" width={250} height={250} />
+          <div className="resume-content px-5">
+            <div className="resume-intro text-center pb-5">
+              <h1 className="text-3xl tracking-wider text-semibold font-semibold text-center">
+                Projects and Testimonials
+              </h1>
+              <p className="text-lg">
+                I will let the people I have previously worked with convince you
+                I am the right fit to work on your project for you.
+              </p>
+              <p className="text-lg">
+                Here is a list of some of the people I have being fortunate
+                enough to work with and what they had to say of their experience
+                of working with me.
+              </p>
+            </div>
+            <div className="testimonial-container">
+              <div className="client-testimonial flex w-11/12 mx-auto mt-5 items-center">
+                <div className="client hidden md:block mx-3">
+                  <Image
+                    src={"/logos/WhiteOnBlack.png"}
+                    alt={"Client Image"}
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="testimonial bg-gray-100 flex flex-col w-11/12 mx-auto my-3 rounded-xl px-2 md:flex-row">
+                  <div className="photo my-auto mx-auto md:w-1/5">
+                    <Image
+                      src={"/logos/WhiteOnBlack.png"}
+                      alt={"Client Image"}
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="words flex flex-col ml-3 pt-1 my-auto md:w-4/5">
+                    <p className="review text-gray-900 font-semibold text-lg">
+                      <span className="text-3xl">“</span>
+                      Denis is the very definition of dedicated customer
+                      service. Over the course of multiple projects I&apos;ve
+                      worked with him on, his diligence, commitment and
+                      expertise have continuously proved invaluable in ensuring
+                      the timely delivery of said projects while always
+                      maintaining his characteristic quality standards. He is
+                      quite the fast learner and his intellectual curiosity is
+                      perhaps one of the most priceless assets he brings into
+                      any developer team.
+                      <span className="text-3xl">”</span>
+                    </p>
+                    <figcaption className="pl-5 text-lg font-medium p-1 md:flex md:justify-between">
+                      <div className="left">
+                        <p className="name  text-gray-700">Cedrique Wekesa</p>
+                        <p className="title text-gray-500 text-sm">
+                          Co-Founder & CEO of Logicae Science
+                        </p>
+                      </div>
+                      <div className=" right text-gray-800 pr-5">
+                        <a href="https://logicae.science/">View Work</a>
+                      </div>
+                    </figcaption>
+                  </div>
+                </div>
+              </div>
+              <div className="client-testimonial flex w-11/12 mx-auto mt-5 items-center">
+                <div className="client hidden md:block mx-3">
+                  <Image
+                    src={"/logos/WhiteOnBlack.png"}
+                    alt={"Client Image"}
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="testimonial bg-gray-100 flex flex-col w-11/12 mx-auto my-3 rounded-xl px-2 md:flex-row">
+                  <div className="photo my-auto mx-auto md:w-1/5">
+                    <Image
+                      src={"/logos/WhiteOnBlack.png"}
+                      alt={"Client Image"}
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="words flex flex-col ml-3 pt-1 my-auto md:w-4/5">
+                    <p className="review text-gray-900 font-semibold text-lg">
+                      <span className="text-3xl">“</span>I recently worked with
+                      Denis to improve my company&apos;s web app. He delivered
+                      everything I asked for in a timely manner .He is extremely
+                      helpful in guiding through the whole process and overall,
+                      I am beyond pleased with Denis&apos;s impeccable service.
+                      <span className="text-3xl">”</span>
+                    </p>
+                    <figcaption className="pl-5 text-lg font-medium p-1 md:flex md:justify-between">
+                      <div className="left">
+                        <p className="name  text-gray-700">Lumona Mulengwa</p>
+                        <p className="title text-gray-500 text-sm">
+                          CTO Sekani
+                        </p>
+                      </div>
+                      <div className=" right text-gray-800 pr-5">
+                        <a href="https://sekani.tech/">View Work</a>
+                      </div>
+                    </figcaption>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </motion.div>
     </div>
   );
 }
--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
   const today = new Date();
   return (
-    <footer className="bg-gray-900 text-gray-300 text-lg font-semibold tracking-wider">
+    <footer className="bg-background text-foreground text-lg font-semibold tracking-wider w-full border-t-2 border-foreground">
       <div className="footer-container flex flex-col items-center p-3 md:flex-row-reverse md:w-11/12 md:mx-auto md:justify-between">
         <p>
           <Link href="/">Back To Top</Link>
