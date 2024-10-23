1e741d821cdfc766189a121ea836fa83afa0dde9
DenisWritesCode <deniswritescode@gmail.com>
2024-10-23 09:39:35 +0300

    002: Add resend and react-emails package and hello-world config

--- emails/contactFromCustomer.tsx
+++ emails/contactFromCustomer.tsx
@@ -1,11 +1,26 @@
+// "use client";
+
 import { Button, Html } from "@react-email/components";
+import { Resend } from 'resend';
 import * as React from "react";
 
-export default function Email() {
+async function sendEmail() {
+  const resend = new Resend('re_123456789');
+
+  await resend.emails.send({
+    from: 'you@example.com',
+    to: 'deniswritescode@gmail.com',
+    replyTo: 'you@example.com',
+    subject: 'hello world',
+    text: 'it works!',
+  });
+}
+
+export default async function Email() {
   return (
     <Html>
       <Button
-        href="https://example.com"
+        onClick={async () => await sendEmail()}
         style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
       >
         Click me
